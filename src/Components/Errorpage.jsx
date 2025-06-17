40a0066d9b992f7eedf0ea71b1404e0a8ae5f961
mahin67580 <mahin67580@gmail.com>
2025-06-17 06:01:49 +0600

    error page added

--- src/Components/Errorpage.jsx
+++ src/Components/Errorpage.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { Link, useRouteError } from 'react-router';
-
+import { Link } from 'react-router';
+import Lottie from "lottie-react";
+import erroranimation from "../assets/error.json";
 const Errorpage = () => {
-    const error = useRouteError()
+    // const error = useRouteError()
     return (
         <div>
             <div>
 
-                <div className='py-24 text-center border-2 w-8/12 mx-auto mt-8'>
-                    <h1 className='mb-8 text-7xl font-bold text-gray-900 italic'>
+                <div className='py-24 text-center   w-8/12 mx-auto mt-8'>
+                    {/* <h1 className='mb-8 text-7xl font-bold text-gray-900 italic'>
                         {error?.status || 404}
                     </h1>
                     <p className='mb-3 text-xl font-bold text-gray-900 md:text-2xl'>
                         {error?.error?.message || 'Something Went Wrong!'}
-                    </p>
+                    </p> */}
+                    <div className='bg-white'>
+                        <Lottie style={{height:"200px"}} animationData={erroranimation} />
+                    </div>
                     <Link to='/'>
                         <a className="btn rounded-full mt-7 text-white bg-[#0EA106]">Go Back Home</a>
                     </Link>
